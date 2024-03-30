69ab67c519ac5f457736f951e73833479945b8da
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-03-30 03:03:28 -0400

    woof

--- flask/Face.py
+++ flask/Face.py
@@ -1,3 +1,20 @@
 import cv2
 import mediapipe as mp
 import time
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+
+while True:
+    success, img = cap.read()
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
+
+
+    cv2.imshow("Hand Detection", img)
+    cv2.waitKey(1)
+
