a3cf1116c2d9bf76dc5aafa577f8e141d5b9bf36
Pedro A. Pajarillo Jr <pedropa140@gmail.com>
2024-03-29 23:35:17 -0400

    chatbot works

--- flask/app.py
+++ flask/app.py
@@ -11,6 +11,7 @@ from datetime import datetime, timezone
 import datetime as dt
 from authlib.integrations.flask_client import OAuth
 
+import google.generativeai as genai
 from google.auth import load_credentials_from_file
 from google.oauth2 import credentials
 from google.auth.transport.requests import Request
@@ -49,6 +50,38 @@ except Exception as e:
 @app.route("/")
 def mainpage():
     return render_template("main.html")
+
+@app.route("/chatbot", methods=["GET", "POST"])
+def chatbot():
+    if request.method == "POST":
+        if not request.form.get("message"):
+            return render_template("error.html")
+
+        userInput = request.form.get("message")
+        print("User Input:", userInput)
+        query = f"As a chatbot, your goal is to help with questions that only pertain into women in the field of STEM. The question the user wants to ask is {userInput}. Please answer the prompt not in markdown please."
+        model = genai.GenerativeModel('models/gemini-pro')
+        result = model.generate_content(query)
+        # print(result.text)
+        formatted_message = ""
+        lines = result.text.split("\n")
+
+        for line in lines:
+            bold_text = ""
+            while "**" in line:
+                start_index = line.index("**")
+                end_index = line.index("**", start_index + 2)
+                bold_text += "<strong>" + line[start_index + 2:end_index] + "</strong>"
+                line = line[:start_index] + bold_text + line[end_index + 2:]
+            formatted_message += line + "<br>"
+        question_response = (userInput, formatted_message)
+
+        print(question_response)
+
+        return render_template("chatbot.html", question_response=question_response)
+    else:
+        question_response = ("", "")
+        return render_template("chatbot.html", question_response=question_response)
 
 if __name__ == "__main__":
     app.run(debug=True)
