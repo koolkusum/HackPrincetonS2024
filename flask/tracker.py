93b0c62a29526b41ef0630897a36b3c71f9c0d5a
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-03-30 06:21:45 -0400

    idea change ig

--- flask/tracker.py
+++ flask/tracker.py
@@ -1,5 +1,25 @@
 import cv2
 import mediapipe as mp
+import time
+
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+
+while True:
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
+
+
+    cv2.imshow("Hand Detection", img)
+    cv2.waitKey(1)
+
+'''
 
 # Initialize MediaPipe Face Detection and Face Mesh (Landmark Detection) models.
 face_detector = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.5)
@@ -49,3 +69,5 @@ while cap.isOpened():
 # Release the video capture object and close OpenCV windows.
 cap.release()
 cv2.destroyAllWindows()
+
+'''
